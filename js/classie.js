06fb62d3fa81baba363940be4f78781cb2e2737b
Dai Phung <dai@daiphung.com>
2017-11-04 12:05:17 +1100

    smooth scrolling

--- js/classie.js
+++ js/classie.js
@@ -80,3 +80,13 @@ if ( typeof define === 'function' && define.amd ) {
 }
 
 })( window );
+
+//smooth scrolling
+// 
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+   }, 500);
+});
